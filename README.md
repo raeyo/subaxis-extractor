dac4319cf9584f2766e506c22b70cd9e7b5f644d
Seunghyeok Back <shback@gist.ac.kr>
2019-11-18 20:52:42 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# subaxis_extractor
+
+detect cylindrical objects from CAD file (.STEP) to propose sub-axis candidates
+
+![](./imgs/img1.png)
+![](./imgs/img2.png)
+![](./imgs/img3.png)
+
+## Requirements
+
+- python 2.7
+- freecad
+- pyqt4
+- argparse
+- tqdm
+- json
+
+## directory sturcture
+
+```
++-- step # input step files
+    +-- stefan
+
++-- hole_info # output sub-axis
+    +-- part1.json
+    +-- part2.json
+
+```
+
+## data format
+
+```
+{
+"file_id": int,
+"file_name": str,
+"EA": int,
+"n_hole": int ,
+"holes": [hole]
+}
+
+hole{
+"hole_id": int,
+"radius": int,
+"position", [x, y, z],
+"x_axis", [nx, ny, nz],
+"y_axis", [nx, ny, nz],
+"axis", [nx, ny, nz],
+}
+
+```
+
+## references
+
+- https://forum.freecadweb.org/viewtopic.php?t=5059
+
+## how-to-use
+
+```
+python detect_holes_from_STEP.py --i {input_folder}, --o {output_folder} --gui 5
+```
---
+++ detect_holes_from_STEP.py
@@ -0,0 +1,133 @@
+from __future__ import print_function
+import sys
+sys.path.append("/usr/lib/freecad/lib/")
+import FreeCAD 
+import FreeCADGui 
+import Part, Mesh
+from FreeCAD import Draft
+from PyQt4 import QtGui,QtCore
+from math import pi
+import random
+import argparse
+import time
+import glob
+from tqdm import tqdm
+import json
+
+def round_all(stuff):
+    if isinstance(stuff, list):
+        return [round_all(x) for x in stuff]
+    if isinstance(stuff, tuple):
+        return tuple(round_all(x) for x in stuff)
+    return round(float(stuff), 2)
+
+def detect_holes(step_file, doc_name):
+
+    # read step file
+    shape = Part.Shape()
+    shape.read(step_file)
+
+    # initialize FreeCAD
+    FreeCADGui.showMainWindow()
+    doc = FreeCAD.newDocument(doc_name)
+    FreeCAD.setActiveDocument(doc_name)
+    FreeCAD.ActiveDocumnet = FreeCAD.getDocument(doc_name)
+    FreeCADGui.ActiveDocument
+
+    # load object
+    pf = doc.addObject("Part::Feature","MyShape")
+    pf.Shape = shape
+    FreeCAD.ActiveDocument.recompute()
+    FreeCADGui.getDocument(doc_name)
+
+    # eliminate flat faces
+    facelist = []
+    for f in pf.Shape.Faces:
+        if f.ParameterRange[1] > 6.28318 and f.ParameterRange[1] < 6.283186 :
+            facelist.append(f)
+
+    # detect holes
+    holes = []
+    hole_id = 0
+    for h in facelist:
+        for w in h.Wires:
+            for c in w.Edges:
+                # find any cylinderical objects
+                if (isinstance(c.Curve, Part.Circle)):
+                    hole = {}
+                    print(c.Curve)
+                    hole["hole_id"] = hole_id
+                    hole["radius"] = c.Curve.Radius
+                    hole["position"] = (c.Curve.Center.x, c.Curve.Center.y, c.Curve.Center.z)
+                    hole["x_axis"] = (c.Curve.XAxis.x, c.Curve.XAxis.y, c.Curve.XAxis.z)
+                    hole["y_axis"] = (c.Curve.YAxis.x, c.Curve.YAxis.y, c.Curve.YAxis.z)
+                    hole["z_axis"] = (c.Curve.Axis.x, c.Curve.Axis.y, c.Curve.Axis.z)
+
+                    holes.append(hole)
+                    hole_id += 1
+
+    if args.gui != 0:
+        for hole in holes:
+            # get z-axis
+            for i, normal in enumerate(hole["z_axis"]):
+                if int(normal) != 0:
+                    z_axis = i
+            pos1 = list(hole["position"])
+            pos2 =list(hole["position"])
+            pos1[z_axis] += 30
+            pos2[z_axis] -= 30
+
+            obj = doc.addObject("Part::Polygon", "Polygon")
+            obj.Nodes = [tuple(pos1), tuple(pos2)]
+
+        FreeCAD.ActiveDocument.recompute()
+        FreeCADGui.getDocument(doc_name)
+        FreeCADGui.showMainWindow()
+
+        view = FreeCADGui.ActiveDocument.ActiveView
+        view.viewAxometric()
+        view.fitAll()
+
+        for i in range(args.gui*20):
+            FreeCADGui.updateGui()
+            time.sleep(0.05)
+
+    return holes
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-i","--input_path", type=str, default='./step/stefan', help="path for the step files")
+    parser.add_argument("-o","--output_path", type=str, default='./hole_info', help="path for the output json file")
+    parser.add_argument("--gui", type=int, default='0', help="enable gui mode for x seconds")
+    parser.add_argument("--doc", type=str, default='doc', help="document name")
+
+    args = parser.parse_args()
+
+    input_paths = glob.glob(args.input_path + '/*')
+    input_paths.sort()
+
+    file_id = 0
+
+    for i, input_path in enumerate(tqdm(input_paths)):
+        hole_info = {}
+        hole_info['file_id'] = file_id
+        file_name = input_path.split('/')[-1].split('.')[0]
+        hole_info['file_name'] = file_name
+
+        if file_name.find('ea)') == -1:
+            hole_info['EA'] = 1
+        else:
+            hole_info['EA'] = int(file_name[file_name.find('(')+1:file_name.find('ea)')])
+
+
+        print("detect {} holes in {}".format(hole_info['EA'], hole_info['file_name']), '\n')
+        holes = detect_holes(input_path, args.doc + str(i))
+        hole_info["hole"] = holes
+        file_id += 1
+
+        # save dictionary into json
+        with open(args.output_path + '/' + file_name + '.json', 'w') as f:
+            json.dump(hole_info, f)
